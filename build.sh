8ed676d5ba89e2166d206aa2ed5d617f1a89adf7
Dominic Lord <dominic.lord@lg2.com>
2025-01-22 12:58:07 -0500

    testing build script

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 # !/bin/bash
 
+# Generate static site
 yarn generate --fail-on-error
 
+# Create zip file
 ZIP_NAME="dist-${CF_PAGES_COMMIT_SHA}.zip"
-echo $ZIP_NAME
 tar -czf "$ZIP_NAME" dist/
 mv "$ZIP_NAME" dist/
+
+# Upload to R2 using wrangler
+wrangler r2 object put "${R2_BUCKET_NAME}/${ZIP_NAME}" --file "dist/$ZIP_NAME"
