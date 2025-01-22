86320200673cc1c82ed2c71258003e151a360da5
Dominic Lord <dominic.lord@lg2.com>
2025-01-22 14:32:29 -0500

    Removed moving of zip backup

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ yarn generate --fail-on-error
 # Create zip file
 ZIP_NAME="dist-${CF_PAGES_COMMIT_SHA}.zip"
 tar -czf "$ZIP_NAME" dist/
-mv "$ZIP_NAME" dist/
 
 # Upload to R2 using wrangler
-npx wrangler r2 object put "${R2_BUCKET_NAME}/${ZIP_NAME}" --file "dist/$ZIP_NAME"
+npx wrangler r2 object put "${R2_BUCKET_NAME}/${ZIP_NAME}" --file "$ZIP_NAME"
